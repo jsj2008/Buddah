86d5caa281d51a71385a5fccf88fd4f04988e232
root <root@domU-12-31-39-04-4A-F4.compute-1.internal>
2012-12-21 07:04:11 +0000

    need to get ajax working

---
+++ html/js/cookies.js
@@ -0,0 +1,26 @@
+function setCookie(c_name, c_value, expire) {
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate()+expire);
+
+	c_name = escape(c_name);
+	c_value = escape(c_value);
+	document.cookie = c_name + "=" + c_value +
+		(expire==null ? "" : ";expires="+exdate.toGMTString());
+}
+
+function getCookie(c_name) {
+	var i, e, d, k, v;
+	var cookies = document.cookie.split(";");
+
+	for (i = 0; i < cookies.length; i++) {
+		e = cookies[i];
+		d = e.indexOf("=");
+		k = e.substr(0, d);
+		v = e.substr(d+1);
+
+		console.log(e);
+		if (c_name == k)
+			return unescape(v);
+	}
+}
+
