653f85dece2286ea1fbe7cb09e02a78f15167d02
Luke Zheng <luke.h.zheng@gmail.com>
2013-01-19 10:55:57 -0800

    modified requests to include username as part of each request aside from initiateRegister

--- backend/requests/LoginRequest.java
+++ backend/requests/LoginRequest.java
@@ -6,6 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class LoginRequest
 {
     private String userToken;
+    private String username;
 
     public LoginRequest()
     {
@@ -19,6 +20,16 @@ public class LoginRequest
     public void setUserToken( String userToken )
     {
         this.userToken = userToken;
+    }
+    
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername( String username )
+    {
+        this.username = username;
     }
 
     public String toString()
