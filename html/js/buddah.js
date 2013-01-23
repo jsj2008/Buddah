49881124187853006f920db39de8e2f4ddd34a43
EC2 Default User <ec2-user@domU-12-31-39-04-4A-F4.compute-1.internal>
2013-01-23 06:37:54 +0000

    oops, git is weird

--- html/js/buddah.js
+++ html/js/buddah.js
@@ -1,67 +1,132 @@
-/*
-function httpRequest(params) {
-	var xmlHttp = new XMLHttpRequest();
+/* done:
+ * 
+ * not done:
+ * getDeals
+ * getDealsResult
+ * rating
+ * posting
+ * viewUserRating
+ * viewUserRatingResult
+ */
 
-	xmlhttp.onreadystatechange = function () {
-
-	} 
-
-	xmlHttp.open(params[method], params[url], params[async]);
-	xmlHttp.send(params[send]);
-}
-*/
-var baseUrl = "http://buddah.herokuapp.com/services";
-
-function initForms() {
-	initiateRegisterInit();
-	getDealsInit();
-}
-
-function initiateRegisterInit() {
-	$('#initiateRegisterRequest').submit( function() {
-		$.ajax({
-			type: "PUT",
-			url: baseUrl + "/initiateRegister",
-			data: $("#initiateRegisterRequest").serialize(),
-			success: function (data) {
-				console.log("success" + data);
-			},
-			error: function(a, b, c) {
-				console.log("fuckburgers");
-				console.log(a.toString());
-				console.log(a + b + c + "");
-			},
-			dataType: 'json'
-			
-		});
-
-		return false;
-	});
-
-}
 
 function getDealsInit() {
 	$('#sq2').click( function() {
 		$.ajax({
 			type: "POST",
 			url: baseUrl + "/getDeals",
-			data: 	{userId: "heinanana",
-				position: { longitude: -121, latitude: 37},
-				xOffset: 1.1,
-				yOffset: 2.2},
+			data: JSON.stringify({userToken: get_userToken(),
+									username: get_username(),
+									xOffset: "x",
+									yOffset: "y",
+									position: {latitude: 47,
+												longitude: -122}
+							}),
 			success: function (data) {
-				console.log("get deals success" + data);
+				console.log("guid is " + data);
 			},
-			error: function(a, b, c) {
-				console.log("deals fuckburgers");
-				console.log(a.toString());
-				console.log(a + b + c + "");
+			error: function() {
+				console.log("fuckburgers");
 			},
-			dataType: 'json'
-			
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
 		});
-
 		return false;
 	});
-
 }
+
+function getDealsResultInit() {
+	$('#getDealsResultRequest').submit( function() {
+		$.ajax({
+			type: "POST",
+			url: baseUrl + "/getDealsResult",
+			data: JSON.stringify($("#getDealsResultRequest").serialize()),
+			success: function (data) {
+				console.log("success" + data);
+			},
+			error: function() {
+				console.log("fuckburgers");
+			},
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
+		});
+		return false;
+	});
+}
+
+function ratingInit() {
+	$('#ratingRequest').submit( function() {
+		$.ajax({
+			type: "POST",
+			url: baseUrl + "/rating",
+			data: JSON.stringify($("#ratingRequest").serialize()),
+			success: function (data) {
+				console.log("success" + data);
+			},
+			error: function() {
+				console.log("fuckburgers");
+			},
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
+		});
+		return false;
+	});
+}
+
+function postingInit() {
+	$('#postingRequest').submit( function() {
+		$.ajax({
+			type: "PUT",
+			url: baseUrl + "/posting",
+			data: JSON.stringify($("#postingRequest").serialize()),
+			success: function (data) {
+				console.log("success" + data);
+			},
+			error: function() {
+				console.log("fuckburgers");
+			},
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
+		});
+		return false;
+	});
+}
+
+function viewUserRatingInit() {
+	$('#viewUserRatingRequest').submit( function() {
+		$.ajax({
+			type: "POST",
+			url: baseUrl + "/viewUserRating",
+			data: JSON.stringify($("#viewUserRatingRequest").serialize()),
+			success: function (data) {
+				console.log("success" + data);
+			},
+			error: function() {
+				console.log("fuckburgers");
+			},
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
+		});
+		return false;
+	});
+}
+
+function viewUserRatingResultInit() {
+	$('#viewUserRatingResultRequest').submit( function() {
+		$.ajax({
+			type: "POST",
+			url: baseUrl + "/viewUserRatingResult",
+			data: JSON.stringify($("#viewUserRatingResultRequest").serialize()),
+			success: function (data) {
+				console.log("success" + data);
+			},
+			error: function() {
+				console.log("fuckburgers");
+			},
+			dataType: 'json',
+			contentType: 'application/json; charset=utf-8'
+		});
+		return false;
+	});
+}
+
